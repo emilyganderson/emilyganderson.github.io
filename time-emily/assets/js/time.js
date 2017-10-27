b18474abf8c3f912172e4310f288d2d4feb336fb
Emily Anderson <EmilyAnderson@dyn-118-139-30-148.its.monash.edu.au>
2017-10-27 12:24:02 +1100

    time

---
+++ time-emily/assets/js/time.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+
+// Global Vars
+  var oddSecond = ' ';
+  var oddMinute = ' ';
+  var halfSecond = ' ';
+  var halfMinute = ' ';
+
+// Update time
+  function update() {
+    var quarter = moment().quarter();
+    var momentSecond = moment().second();
+    var momentMinute = moment().minute();
+    var momentHour= moment().format('h').toLowerCase();
+    var momentDay = moment().format('dddd').toLowerCase();
+    var momentMonth = moment().format('MMMM').toLowerCase();
+    var momentYear = moment().format('YYYY').toLowerCase();
+    var momentAMPM = moment().format('A').toLowerCase();
+
+    function oddOrEvenS(momentSecond){
+      if(momentSecond % 2 === 0) {oddSecond = 'oddS';}
+      else {oddSecond = 'evenS';}
+    }
+
+    function oddOrEvenM(momentMinute){
+      if(momentMinute % 2 === 0) {oddMinute = 'evenM';}
+      else {oddMinute = 'oddM';}
+    }
+
+    function checkHalfSecond(momentSecond){
+      if(momentSecond < 30) {halfSecond = 'halfM';}
+      else {halfSecond = 'fullM';}
+    }
+
+    function checkHalfMinute(momentMinute){
+        if(momentMinute < 5) {halfMinute = 'everyFive';}
+        else if(momentMinute < 10){halfMinute = 'everyTen';}
+        else if(momentMinute < 15){halfMinute = 'everyFifteen';}
+        else if(momentMinute < 30){halfMinute = 'halfH';}
+        else if(momentMinute < 45){halfMinute = '';}
+        else {$('.container').attr('class', 'container');}
+      }
+
+    // Check if odd, even, quaterpast
+    oddOrEvenS(momentSecond);
+    checkHalfSecond(momentSecond);
+    oddOrEvenM(momentMinute);
+    checkHalfMinute(momentMinute);
+
+    var timeAll = ['s' + momentSecond, 'm' + momentMinute, 'h' + momentHour, momentAMPM, momentDay, momentMonth, 'y' + momentYear, oddSecond, oddMinute, halfSecond, halfMinute];
+    var timeClass = timeAll.join(' ');
+
+    // Add classes
+    $('body').attr('class', timeClass);
+    $('.container').addClass('m' + momentMinute);
+
+    // Add format
+    $('.time').html(moment().format('dddd D MMMM YYYY h:mm:ss A'));
+  };
+
+  var intervalId = setInterval(update, 100);
+  intervalId
+
+// Add pause
+  $('button#pause').click(function(){
+    clearInterval(intervalId);
+    $('textarea#timeClasses').show();
+  });
+
+});
